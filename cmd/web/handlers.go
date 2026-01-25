3a6c9de4b8de4fe49b28e9227829a47f423c8855
Nurkhuzhayeva Shinaray <241243@astanait.edu.kz>
2026-01-25 19:57:41 +0500

    web1 process

---
+++ cmd/web/handlers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Welcome to Kazakh Aliexpress - Best deals only here!"))
+}
+
+func (app *application) showProduct(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	fmt.Fprintf(w, "Displaying details for Product ID: %d", id)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/product", app.showProduct)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
